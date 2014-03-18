3cb9f31868d6b8e6785da4a81f6233595a1d63ba
aitorneos <aitormenta71@gmail.com>
2014-03-18 13:00:53 +0100

    v 4.2 options menu final version

--- src/Scene/OptionsScene.java
+++ src/Scene/OptionsScene.java
@@ -24,6 +24,11 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	private boolean activated2 = false;
 	private boolean arSel = false;
 	
+	IMenuItem player1Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(1, resourcesManager.player1, vbom), 1.2f, 1);;
+	IMenuItem player2Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(2, resourcesManager.player2, vbom), 1.2f, 1);;
+	IMenuItem player3Sel= new ScaleMenuItemDecorator(new SpriteMenuItem(3, resourcesManager.player3, vbom), 1.2f, 1);;
+	IMenuItem player4Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(4, resourcesManager.player4, vbom), 1.2f, 1);;
+	
 	private final int MENU_PLAYER_SEL = 0;
 	private final int PLAYER1_SEL = 1;
 	private final int PLAYER2_SEL = 2;
@@ -105,6 +110,7 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	{
 		ResourcesManager.getInstance().unloadOptionsScreen();
 		SceneManager.getInstance().loadMenuScene(engine);
+		disposeScene();
 		
 	}
 
@@ -114,8 +120,7 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 		return SceneType.SCENE_OPTIONS;
 	}
 
-	 @Override
-    public void disposeScene(int levelID)
+    public void disposeScene()
     {
     }
 
@@ -127,7 +132,7 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	    {
 		 
 	        case MENU_PLAYER_SEL:
-	        	
+
 	            //Load Corresponding texture ---> player selected
 	        	switch (player_sel)
 	        	{
@@ -138,8 +143,9 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 		        		ResourcesManager.getInstance().controlLoaded = true;
 		        		resourcesManager.playerSelected = 0; 
 		        		menuChildScene.clearChildScene();
-		        		createScene();	    
-		       	     	final IMenuItem player1Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(PLAYER1_SEL, resourcesManager.player1, vbom), 1.2f, 1);
+		        		menuChildScene.reset();
+		        		player4Sel.reset();
+		       	     	player1Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(PLAYER1_SEL, resourcesManager.player1, vbom), 1.2f, 1);
 		       	     	menuChildScene.addMenuItem(player1Sel);
 		       	     	player1Sel.setPosition(player1Sel.getX() + 875 , player1Sel.getY() + 200);
 		        		break;
@@ -148,10 +154,11 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 		        		
 		        		player_sel = 2;
 		        		resourcesManager.playerSelected = 1;
-		        		ResourcesManager.getInstance().controlLoaded = true;
+		        		ResourcesManager.getInstance().controlLoaded = true; 
 		        		menuChildScene.clearChildScene();
-		        		createScene();	        		
-		       	     	final IMenuItem player2Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(PLAYER2_SEL, resourcesManager.player2, vbom), 1.2f, 1);
+		        		menuChildScene.reset();
+		        		player1Sel.reset();
+		       	     	player2Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(PLAYER2_SEL, resourcesManager.player2, vbom), 1.2f, 1);
 		       	     	menuChildScene.addMenuItem(player2Sel);
 		       	     	player2Sel.setPosition(player2Sel.getX() + 875 , player2Sel.getY() + 200);
 		        		break;
@@ -160,10 +167,11 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 		        		
 		        		player_sel = 3;
 		        		resourcesManager.playerSelected = 2;
-		        		ResourcesManager.getInstance().controlLoaded = true;
+		        		ResourcesManager.getInstance().controlLoaded = true;   
 		        		menuChildScene.clearChildScene();
-		        		createScene();	        		
-		       	     	final IMenuItem player3Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(PLAYER3_SEL, resourcesManager.player3, vbom), 1.2f, 1);
+		        		menuChildScene.reset();
+		        		player2Sel.reset();
+		       	     	player3Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(PLAYER3_SEL, resourcesManager.player3, vbom), 1.2f, 1);
 		       	     	menuChildScene.addMenuItem(player3Sel);
 		       	     	player3Sel.setPosition(player3Sel.getX() + 875 , player3Sel.getY() + 200);
 		        		break;
@@ -172,10 +180,11 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 		        		
 		        		player_sel = 0;
 		        		resourcesManager.playerSelected = 3;
-		        		ResourcesManager.getInstance().controlLoaded = true;
+		        		ResourcesManager.getInstance().controlLoaded = true;  
 		        		menuChildScene.clearChildScene();
-		        		createScene();      		
-		       	     	final IMenuItem player4Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(PLAYER4_SEL, resourcesManager.player4, vbom), 1.2f, 1);
+		        		menuChildScene.reset();
+		        		player3Sel.reset();
+		       	     	player4Sel = new ScaleMenuItemDecorator(new SpriteMenuItem(PLAYER4_SEL, resourcesManager.player4, vbom), 1.2f, 1);
 		       	     	menuChildScene.addMenuItem(player4Sel);
 		       	     	player4Sel.setPosition(player4Sel.getX() + 875 , player4Sel.getY() + 200);
 		        		break;
@@ -193,8 +202,8 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	        	final IMenuItem notSel3 = new ScaleMenuItemDecorator(new SpriteMenuItem(NOT, resourcesManager.not, vbom), 1, 1);
 	        	if (arSel)
 	        	{
-	       	     	yesSel3.setPosition(yesSel3.getX() + 800 , yesSel3.getY() - 50);
-	       	     	notSel3.setPosition(notSel3.getX() + 900 , notSel3.getY() - 50);
+	       	     	yesSel3.setPosition(yesSel3.getX() + 850 , yesSel3.getY() + 125);
+	       	     	notSel3.setPosition(notSel3.getX() + 950 , notSel3.getY() + 125);
 	       	     	notSel3.setColor(Color.RED);
 	       	     	yesSel3.setColor(Color.WHITE);
 	        		menuChildScene.addMenuItem(yesSel3);
@@ -202,14 +211,14 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	        	}
 	        	else
 	        	{
-	       	     	notSel3.setPosition(notSel3.getX() + 900 , notSel3.getY() - 50);
-	       	     	yesSel3.setPosition(yesSel3.getX() + 800 , yesSel3.getY() - 50);
+	       	     	notSel3.setPosition(notSel3.getX() + 950 , notSel3.getY() + 125);
+	       	     	yesSel3.setPosition(yesSel3.getX() + 850 , yesSel3.getY() + 125);
 	       	     	yesSel3.setColor(Color.RED);
 	       	     	notSel3.setColor(Color.WHITE);
 	        		menuChildScene.addMenuItem(notSel3);
 	        		menuChildScene.addMenuItem(yesSel3);
 	        	}
-    			return true;
+    			break;
 	        			
 	        case LEVEL_SEL:
 	        	
@@ -259,6 +268,7 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	        	
 	        	// Assign level selected to resources manager variable
 	        	ResourcesManager.getInstance().setLevelComplete((ResourcesManager.getInstance().getLevelComplete() % 4) + 1);
+	        	return true;
 	        	
 	        case SHADER:
 	        	
@@ -268,8 +278,8 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	        	final IMenuItem notSel = new ScaleMenuItemDecorator(new SpriteMenuItem(NOT, resourcesManager.not, vbom), 1, 1);
 	        	if (activated)
 	        	{
-	       	     	yesSel.setPosition(yesSel.getX() + 800 , yesSel.getY() - 50);
-	       	     	notSel.setPosition(notSel.getX() + 900 , notSel.getY() - 50);
+	       	     	yesSel.setPosition(yesSel.getX() + 850 , yesSel.getY() - 75);
+	       	     	notSel.setPosition(notSel.getX() + 950 , notSel.getY() - 75);
 	       	     	notSel.setColor(Color.RED);
 	       	     	yesSel.setColor(Color.WHITE);
 	        		menuChildScene.addMenuItem(yesSel);
@@ -277,15 +287,15 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	        	}
 	        	else
 	        	{
-	       	     	notSel.setPosition(notSel.getX() + 900 , notSel.getY() - 50);
-	       	     	yesSel.setPosition(yesSel.getX() + 800 , yesSel.getY() - 50);
+	       	     	notSel.setPosition(notSel.getX() + 950 , notSel.getY() - 75);
+	       	     	yesSel.setPosition(yesSel.getX() + 850 , yesSel.getY() - 75);
 	       	     	yesSel.setColor(Color.RED);
 	       	     	notSel.setColor(Color.WHITE);
 	        		menuChildScene.addMenuItem(notSel);
 	        		menuChildScene.addMenuItem(yesSel);
 	        	}
 	        	
-	        	return true;
+	        	break;
 	        	
 	        case PARTICLE:
 	        	
@@ -295,8 +305,8 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	        	activated2 = !activated2;
 	        	if (activated2)
 	        	{
-	       	     	yesSel2.setPosition(yesSel2.getX() + 800 , yesSel2.getY() - 125);
-	       	     	notSel2.setPosition(notSel2.getX() + 900 , notSel2.getY() - 125);
+	       	     	yesSel2.setPosition(yesSel2.getX() + 850 , yesSel2.getY() - 130);
+	       	     	notSel2.setPosition(notSel2.getX() + 950 , notSel2.getY() - 130);
 	       	     	notSel2.setColor(Color.RED);
 	       	     	yesSel2.setColor(Color.WHITE);
 	        		menuChildScene.addMenuItem(yesSel2);
@@ -304,19 +314,26 @@ public class OptionsScene extends BaseScene implements IOnMenuItemClickListener
 	        	}
 	        	else
 	        	{
-	       	     	notSel2.setPosition(notSel2.getX() + 900 , notSel2.getY() - 125);
-	       	     	yesSel2.setPosition(yesSel2.getX() + 800 , yesSel2.getY() - 125);
+	       	     	notSel2.setPosition(notSel2.getX() + 950 , notSel2.getY() - 130);
+	       	     	yesSel2.setPosition(yesSel2.getX() + 850 , yesSel2.getY() - 130);
 	       	     	yesSel2.setColor(Color.RED);
 	       	     	notSel2.setColor(Color.WHITE);
 	        		menuChildScene.addMenuItem(notSel2);
 	        		menuChildScene.addMenuItem(yesSel2);
 	        	}
 	        	
-	        	return true;
+	        	break;
 	            
 	        default:
-	            return false;
+	         break;
 	    }
+		return true;
+	}
+
+	@Override
+	public void disposeScene(int levelID)
+	{
+		
 	}
 
 }
